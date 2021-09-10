21e6d9f2c167a8dd9ba79e18c88ed8ae49431817
Allen Francis <lallenfrancisl@gmail.com>
2021-09-10 23:54:18 +0530

    fix: routes hitting 404 when trailing slash is added

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import Hapi from '@hapi/hapi';
-import status from './plugins/status';
-import prismaPlugin from './plugins/prisma';
+import Hapi from "@hapi/hapi";
+import status from "./plugins/status";
+import prismaPlugin from "./plugins/prisma";
+import usersPlugin from "./plugins/users";
 
 const server: Hapi.Server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || "localhost",
+  router: {
+    stripTrailingSlash: true,
+  }
 });
 
 export async function createServer(): Promise<Hapi.Server> {
-  await server.register([status, prismaPlugin]);
+  await server.register([status, prismaPlugin, usersPlugin]);
   await server.initialize();
 
   return server;
@@ -21,7 +25,7 @@ export async function startServer(server: Hapi.Server): Promise<Hapi.Server> {
   return server;
 }
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
