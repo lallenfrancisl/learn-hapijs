ee0470d292e7e2448435221aa5c1d4ba56c6c89b
Allen Francis <lallenfrancisl@gmail.com>
2021-09-11 01:34:33 +0530

    test: add test for data validation

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -13,6 +13,8 @@ describe('POST /users - register user', () => {
   });
 
   let userId: number;
+
+  // test if the route works
   test('create user', async () => {
     const res = await server.inject({
       method: 'POST',
@@ -31,5 +33,23 @@ describe('POST /users - register user', () => {
     expect(res.statusCode).toEqual(201);
     userId = JSON.parse(res.payload).data?.id;
     expect(typeof userId === 'number').toBeTruthy();
+  });
+
+  // test if data validation works
+  test('register user validation', async () => {
+    const response = await server.inject({
+      method: 'POST',
+      url: '/users',
+      payload: {
+        lastName: 'test-last-name',
+        email: `test-${Date.now()}@prisma.io`,
+        social: {
+          twitter: 'thisisalice',
+          website: 'https://www.thisisalice.com',
+        },
+      },
+    });
+
+    expect(response.statusCode).toEqual(400);
   });
 });
