2c3678ec30d9e6e2a4e4b12d67c403c5828b21e6
Allen Francis <lallenfrancisl@gmail.com>
2021-09-10 22:56:59 +0530

    style: format using the updated prettier config

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from "@prisma/client";
-import Hapi from "@hapi/hapi";
+import { PrismaClient } from '@prisma/client';
+import Hapi from '@hapi/hapi';
 
 // TODO: TBH this is weird, search for alternatives to module
 // augmentation
-declare module "@hapi/hapi" {
+declare module '@hapi/hapi' {
   interface ServerApplicationState {
     prisma: PrismaClient;
   }
 }
 
 const prismaPlugin: Hapi.Plugin<null> = {
-  name: "prisma",
+  name: 'prisma',
   register: async (server: Hapi.Server) => {
     const prisma = new PrismaClient();
 
@@ -19,7 +19,7 @@ const prismaPlugin: Hapi.Plugin<null> = {
     // Related issue: https://github.com/hapijs/hapi/issues/2839
     // stop the prisma client after the server is stopped
     server.ext({
-      type: "onPostStop",
+      type: 'onPostStop',
       method: async (server: Hapi.Server) => {
         server.app.prisma.$disconnect();
       },
