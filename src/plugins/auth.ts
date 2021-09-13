4150351b2599db0768440a7797a6c2ac32d443aa
Allen Francis <lallenfrancisl@gmail.com>
2021-09-13 17:50:11 +0530

    feat: implement jwt authentication POST /auth

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -6,10 +6,6 @@ import { unauthorized, badImplementation } from '@hapi/boom';
 import { add } from 'date-fns';
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'JWT_SECRET';
-const JWT_ALGORITHM = 'HS256';
-const AUTH_TOKEN_EXP_HOURS = 12;
-
 const authPlugin: Hapi.Plugin<null> = {
   name: 'app/auth',
   dependencies: ['prisma', 'hapi-auth-jwt2', 'app/email'],
@@ -24,6 +20,20 @@ const authPlugin: Hapi.Plugin<null> = {
           validate: {
             payload: Joi.object({
               email: Joi.string().email().required(),
+            }),
+          },
+        },
+      },
+      {
+        method: 'POST',
+        path: '/auth',
+        handler: authHandler,
+        options: {
+          auth: false,
+          validate: {
+            payload: Joi.object({
+              email: Joi.string().email().required(),
+              emailToken: Joi.string().required(),
             }),
           },
         },
@@ -75,6 +85,81 @@ async function loginHandler(req: Hapi.Request, h: Hapi.ResponseToolkit) {
 // Generate a random 8 digit number as the email token
 function generateEmailToken(): string {
   return Math.floor(10000000 + Math.random() * 90000000).toString();
+}
+
+const JWT_SECRET = process.env.JWT_SECRET || 'JWT_SECRET';
+const JWT_ALGORITHM = 'HS256';
+const AUTH_TOKEN_EXP_HOURS = 12;
+
+async function authHandler(req: Hapi.Request, h: Hapi.ResponseToolkit) {
+  const { prisma } = req.server.app;
+  const { email, emailToken } = req.payload as AuthenticateInput;
+
+  try {
+    const fetchedEmailToken = await prisma.token.findUnique({
+      where: {
+        emailToken: emailToken,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!fetchedEmailToken?.valid) {
+      return unauthorized();
+    }
+
+    if (fetchedEmailToken.expiration < Date.now()) {
+      return unauthorized('Token expired');
+    }
+
+    if (fetchedEmailToken?.user?.email === email) {
+      const tokenExpiration = add(new Date(), {
+        hours: AUTH_TOKEN_EXP_HOURS,
+      });
+
+      const createdToken = await prisma.token.create({
+        data: {
+          type: TokenType.API,
+          expiration: tokenExpiration,
+          user: {
+            connect: {
+              email,
+            },
+          },
+        },
+      });
+
+      // the email token must be invalidated after it is used
+      //
+      await prisma.token.update({
+        where: {
+          id: fetchedEmailToken.id,
+        },
+        data: {
+          valid: false,
+        },
+      });
+
+      const authToken = generateAuthToken(createdToken.id);
+      return h.response().code(200).header('Authorization', authToken);
+    }
+
+    return unauthorized();
+  } catch (err) {
+    console.error(err);
+    return badImplementation();
+  }
+}
+
+// Generate a signed JWT token with the tokenId in the payload
+function generateAuthToken(tokenId: number): string {
+  const jwtPayload = { tokenId };
+
+  return jwt.sign(jwtPayload, JWT_SECRET, {
+    algorithm: JWT_ALGORITHM,
+    noTimestamp: true,
+  });
 }
 
 export default authPlugin;
