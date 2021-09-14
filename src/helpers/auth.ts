3b38df94bf9900a637799d5b43fd0283872d8601
Allen Francis <lallenfrancisl@gmail.com>
2021-09-14 22:47:49 +0530

    feat: implement the teacher or admin checker helper

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -14,3 +14,18 @@ export async function isRequestedUserOrAdmin(req: Hapi.Request, h: Hapi.Response
   throw forbidden();
 }
 
+export async function  isTeacherOfCourseOrAdmin(req: Hapi.Request, h: Hapi.ResponseToolkit) {
+  const { isAdmin, teacherOf } = req.auth.credentials;
+
+  if (isAdmin) {
+    return h.continue;
+  }
+
+  const courseId = parseInt(req.params.courseId, 10);
+
+  if ((teacherOf as number[])?.includes(courseId)) {
+    return h.continue;
+  }
+
+  throw forbidden();
+}
